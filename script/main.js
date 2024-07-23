6eaa966661079ffa4872eb1e228b8c25bc16b26d
RianTJackson <RianTJackson@protonmail.com>
2024-07-23 13:22:16 -0400

    page Upload V1.0

---
+++ script/main.js
@@ -0,0 +1,20 @@
+function clownActive() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("clown").style.display = "block";
+    document.getElementById("lioden").style.display = "none";
+    document.getElementById("portPage").style.display = "none";
+  }
+
+  function liodenActive() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("clown").style.display = "none";
+    document.getElementById("lioden").style.display = "block";
+    document.getElementById("portPage").style.display = "none";
+  }
+
+  function portfolioActive() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("clown").style.display = "none";
+    document.getElementById("lioden").style.display = "none";
+    document.getElementById("portPage").style.display = "block";
+  }
